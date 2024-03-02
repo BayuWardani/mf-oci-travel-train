14402a7856a57a45e20beb95448dacdc57a2b432
Bayu Wardani <bayuwardani@Bayus-MacBook-Air.local>
2024-03-02 10:39:55 +0700

    init

---
+++ src/root.component.tsx
@@ -0,0 +1,20 @@
+import {Card} from '@oci-lib/ui'
+
+import '@oci-lib/ui/dist/style.css'
+
+export default function Root(props) {
+  return (
+    <>
+    <div className='d-flex align-item-center justify-content-center mt-4'>
+      <div className='col-md-4'>
+        <Card title="Peringatan">
+          <p>Pesan yang masuk sini adalah : <strong>Hello MF Train</strong></p>
+          <small>
+            <a href='/'>Dashboard</a>
+          </small>
+        </Card>
+      </div>
+    </div>
+    </>
+  )
+}
